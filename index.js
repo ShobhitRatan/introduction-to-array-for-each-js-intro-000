70b5c0bca3da3d712a6e1875875f6b294f82bd65
Shobhit Ratan <shobhit.ratan@baruchmail.cuny.edu>
2020-02-18 00:08:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,7 @@ function doToElementsInArray(array, callback) {
 function changeCompletely(array, index, element) {
   array[index] = Math.floor(Math.random() * 100 + 2).toString() + `${array[index]}s!!!`;
 }
+
+var animals;
+doToElementsInArray(animals, changeCompletely);
+console.log(animals);
